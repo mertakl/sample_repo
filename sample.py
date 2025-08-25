b8498500623bba2e12681fb7a435dcc40e1a02ff
Mert Akel <akelmert95@gmail.com>
2025-08-25 11:36:15 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,22 @@
 def body_element(filepath: str) -> list[docx.oxml.text.paragraph.CT_P]:
     """Return body element of a docx filepath used to get xml."""
     return docx.Document(filepath)._body._body.xpath(".//w:p")  # pylint: disable=W0212
+	
+def depth_from_xml(xml: str) -> int | None:
+    """Get depth from XML.
+
+    Args:
+        xml: XmlString object of the body element
+
+    Returns:
+        depth if any or None
+    """
+    if '<w:pStyle w:val="Heading"/>' in xml or '<w:pStyle w:val="Subtitle"/>' in xml:  # main titles
+        return 0
+    match_depth = re.search(r'<w:pStyle w:val="Contents([1-9]\d*)"', xml)  # depth
+    if match_depth and "__RefHeading___Toc" in xml:  # text in ToC
+        return int(match_depth.group(1))
+    return None  # not main titles nor ToC
 
 ef get_titles(self, filepath: str) -> dict[str, int]:
         """Extracts the titles of the document.

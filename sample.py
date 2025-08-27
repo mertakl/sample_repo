2f82453a98a460babe385ede8c5586124e475cd0
Mert Akel <akelmert95@gmail.com>
2025-08-27 14:40:54 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,6 @@
 default:
   tags:
     - cipcustana
-
 # You can override the included template(s) by including variable overrides
 # SAST customization: https://docs.gitlab.com/ee/user/application_security/sast/#customizing-the-sast-settings
 # Secret Detection customization: https://docs.gitlab.com/ee/user/application_security/secret_detection/#customizing-settings
@@ -9,7 +8,6 @@ default:
 # Container Scanning customization: https://docs.gitlab.com/ee/user/application_security/container_scanning/#customizing-the-container-scanning-settings
 # Note that environment variables can be set in several places
 # See https://docs.gitlab.com/ee/ci/variables/#cicd-variable-precedence
-
 variables:
   DOCKER_IMAGE_NAME: "aidi/aidi-ta02"
   DOCKER_IMAGE_NAME_CANON: "aidita02"
@@ -33,30 +31,31 @@ variables:
   GIT_FETCH_EXTRA_FLAGS: "--depth 200"
 
 stages:
-  - test
-  - release
+- test
+- release
 
 workflow:
   rules:
     # no pipeline for commit tag
     - if: $CI_COMMIT_TAG
       when: never
-    # runs if merging to default branch
+    # runs if merging to master
     - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
-    # don't run on default branch (except if it's merging)
+    # don't run on master (except if it's merging)
     - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH
       when: never
     # run on branches
     - if: $CI_COMMIT_BRANCH
+
 
 test_image:
   stage: test
   script:
     - DOCKER_BUILDKIT=1 docker build . -t ${DOCKER_IMAGE_NAME}:${CI_COMMIT_SHA}
     - echo ${DOCKER_IMAGE_NAME}:${CI_COMMIT_SHA}
-  needs: []
+  needs: [ ]
   rules:
-    # don't run on default branch during merging
+    # don't run on master during merging
     - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
       when: never
     # run on branches
@@ -66,13 +65,13 @@ all_tests:
   stage: test
   image: p-3096-docker-local.artifactory-dogen.group.echonet/icpy/code-quality:uv
   script:
-    # code quality
+    #code quality
     - '[[ -v CI_MERGE_REQUEST_SOURCE_BRANCH_NAME ]] && FROM_REF="$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME" || FROM_REF="$CI_DEFAULT_BRANCH"'
     - git fetch origin $FROM_REF
     - uv run --dev --no-cache pre-commit run --from-ref "origin/$FROM_REF" --to-ref HEAD --show-diff-on-failure
     - uv run --dev --no-cache ruff check --output-format=gitlab > gl-code-quality-report.json
-    
-    # testing & code coverage
+
+    #testing & code coverage
     - echo "The current branch is $FROM_REF"
     - uv run --group test --no-cache bash -c "
       coverage erase &&
@@ -98,15 +97,15 @@ all_tests:
       - pylint-report.txt
   needs: []
   rules:
-    # don't run on default branch during merging
-    - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
-      when: never
-    # run on branches
-    - if: $CI_COMMIT_BRANCH
+      # don't run on master during merging
+      - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
+        when: never
+      # run on branches
+      - if: $CI_COMMIT_BRANCH
 
 test_sonar:
   stage: test
-  needs: []
+  needs: [ ]
   image: "${SONAR_SCANNER_IMAGE}"
   allow_failure: true
   before_script: # to override the global before_script
@@ -129,17 +128,18 @@ test_sonar:
       -Dsonar.scm.disabled=False
       -Dsonar.scm.provider=git
   rules:
-    # don't run on default branch during merging
-    - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
-      when: never
-    # run on branches
-    - if: $CI_COMMIT_BRANCH
+      # don't run on master during merging
+      - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
+        when: never
+      # run on branches
+      - if: $CI_COMMIT_BRANCH
+
 
 pre_release_last_check:
   stage: release
   image: p-3096-docker-local.artifactory-dogen.group.echonet/icpy/code-quality:uv
   script:
-    # code quality
+    #code quality
     - FROM_REF=$CI_DEFAULT_BRANCH
     - echo "The current branch is $(git branch --show-current)"
     - git fetch origin $FROM_REF
@@ -148,32 +148,33 @@ pre_release_last_check:
       export DJANGO_SETTINGS_MODULE=django_project.settings.local &&
       python manage.py migrate &&
       coverage run -m pytest tests/unit tests/functional"
-  needs: []
-  rules:
-    # only run on default branch during merging
-    - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
+  needs: [ ]
+  only:
+    - master
 
 trigger_jenkins:
   stage: release
+
   script:
     - >
       set -xv &&
       wget -S --header='Content-Type: application/json' --post-data "{
-        \"PROJECT\":\"$CI_PROJECT_PATH\",
-        \"APPLICATION_CODE\":\"$APP_NAME\",
-        \"ENABLE_NEXUS_IQ\":\"$ENABLE_NEXUS_IQ\",
-        \"ENABLE_FORTIFY\":\"$ENABLE_FORTIFY\",
-        \"PUBLISH_GITLAB\":\"$PUBLISH_GITLAB\",
-        \"PUBLISH_DOCKER\":\"$PUBLISH_DOCKER\",
-        \"APP_NAME\":\"$APP_NAME\",
-        \"MARVIN_AP_CODE\":\"$MARVIN_AP_CODE\",
-        \"APCODE\":\"$APCODE\",
-        \"DESCRIPTION\":\"$DESCRIPTION\",
-        \"MARVIN_DEV_ENV\":\"$MARVIN_DEV_ENV\",
-        \"ENABLE_RELEASE_MARVIN\":\"$ENABLE_RELEASE_MARVIN\"
-      }" $JENKINS_URL/generic-webhook-trigger/invoke?token=$JENKINS_MARVIN_TOKEN
+          \"PROJECT\":\"$CI_PROJECT_PATH\",
+          \"APPLICATION_CODE\":\"$APP_NAME\",
+          \"ENABLE_NEXUS_IQ\":\"$ENABLE_NEXUS_IQ\",
+          \"ENABLE_FORTIFY\":\"$ENABLE_FORTIFY\",
+          \"PUBLISH_GITLAB\":\"$PUBLISH_GITLAB\",
+          \"PUBLISH_DOCKER\":\"$PUBLISH_DOCKER\",
+          \"APP_NAME\":\"$APP_NAME\",
+          \"MARVIN_AP_CODE\":\"$MARVIN_AP_CODE\",
+          \"APCODE\":\"$APCODE\",
+          \"DESCRIPTION\":\"$DESCRIPTION\",
+          \"MARVIN_DEV_ENV\":\"$MARVIN_DEV_ENV\",
+          \"ENABLE_RELEASE_MARVIN\":\"$ENABLE_RELEASE_MARVIN\"
+        }" $JENKINS_URL/generic-webhook-trigger/invoke?token=$JENKINS_MARVIN_TOKEN
+  only:
+    - master
   needs:
     - pre_release_last_check
-  rules:
-    # only run on default branch during merging
-    - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
+#  needs:
+#    - job: semantic_release

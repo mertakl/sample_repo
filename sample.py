000cc39f10fe8879af23947d6dd89e2e076170a7
Mert Akel <akelmert95@gmail.com>
2025-08-27 14:44:10 +0200

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,23 +1,36 @@
-##Can you refactor and streamline? Also pre_release does not seem to be triggered when merged. Can you fix?
+# ==========================================================================
+# Reusable CI components using YAML anchors
+# ==========================================================================
+.job_templates:
+  # Rules for jobs that should only run on feature branches (NOT on merge to master)
+  test_jobs_rules: &test_jobs_rules
+    rules:
+      # This rule prevents the job from running in the release pipeline triggered by a merge.
+      - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
+        when: never
+      # This rule runs the job for all other branch commits.
+      - if: $CI_COMMIT_BRANCH
 
+  # Rules for jobs that should ONLY run on merge to the default branch
+  release_jobs_rules: &release_jobs_rules
+    rules:
+      # This rule ensures the job ONLY runs when a merge commit is pushed to the default branch.
+      # This is the fix for the original issue.
+      - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
+
+# ==========================================================================
+# Default settings and global variables
+# ==========================================================================
 default:
   tags:
     - cipcustana
-# You can override the included template(s) by including variable overrides
-# SAST customization: https://docs.gitlab.com/ee/user/application_security/sast/#customizing-the-sast-settings
-# Secret Detection customization: https://docs.gitlab.com/ee/user/application_security/secret_detection/#customizing-settings
-# Dependency Scanning customization: https://docs.gitlab.com/ee/user/application_security/dependency_scanning/#customizing-the-dependency-scanning-settings
-# Container Scanning customization: https://docs.gitlab.com/ee/user/application_security/container_scanning/#customizing-the-container-scanning-settings
-# Note that environment variables can be set in several places
-# See https://docs.gitlab.com/ee/ci/variables/#cicd-variable-precedence
+
 variables:
   DOCKER_IMAGE_NAME: "aidi/aidi-ta02"
-  DOCKER_IMAGE_NAME_CANON: "aidita02"
+  DOCKAR_IMAGE_NAME_CANON: "aidita02"
   DOCKER_IMAGE_PORT: "8080"
   MARVIN_URL_PREFIX: "component/aidita02"
-  # comma separated list of folders containing source code for analysis
   SRC_FOLDERS: "moniteur_belge_api,django_project"
-  # comma seperated list of folders containing source code for tests
   SRC_TEST_FOLDERS: "tests"
   COVERAGE_MIN_PC: 0
   ENABLE_NEXUS_IQ: "true"
@@ -33,56 +46,56 @@ variables:
   GIT_FETCH_EXTRA_FLAGS: "--depth 200"
 
 stages:
-- test
-- release
+  - test
+  - release
 
+# ==========================================================================
+# Pipeline creation logic
+# ==========================================================================
 workflow:
   rules:
-    # no pipeline for commit tag
-    - if: $CI_COMMIT_TAG
-      when: never
-    # runs if merging to master
+    # Rule 1: Run a pipeline for merges to the default branch (for releases).
     - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
-    # don't run on master (except if it's merging)
+    # Rule 2: Do NOT run a pipeline for any other commit to the default branch.
     - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH
       when: never
-    # run on branches
+    # Rule 3: Do NOT run pipelines for tags.
+    - if: $CI_COMMIT_TAG
+      when: never
+    # Rule 4: Run a pipeline for all other branches (e.g., feature branches).
     - if: $CI_COMMIT_BRANCH
 
-
+# ==========================================================================
+# Test Stage Jobs (run on feature branches)
+# ==========================================================================
 test_image:
   stage: test
   script:
     - DOCKER_BUILDKIT=1 docker build . -t ${DOCKER_IMAGE_NAME}:${CI_COMMIT_SHA}
     - echo ${DOCKER_IMAGE_NAME}:${CI_COMMIT_SHA}
-  needs: [ ]
-  rules:
-    # don't run on master during merging
-    - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
-      when: never
-    # run on branches
-    - if: $CI_COMMIT_BRANCH
+  needs: []
+  <<: *test_jobs_rules # Apply rules for test jobs
 
 all_tests:
   stage: test
   image: p-3096-docker-local.artifactory-dogen.group.echonet/icpy/code-quality:uv
   script:
-    #code quality
+    # Determine the base branch for comparison (either merge request source or default branch)
     - '[[ -v CI_MERGE_REQUEST_SOURCE_BRANCH_NAME ]] && FROM_REF="$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME" || FROM_REF="$CI_DEFAULT_BRANCH"'
     - git fetch origin $FROM_REF
+    # Code quality checks
     - uv run --dev --no-cache pre-commit run --from-ref "origin/$FROM_REF" --to-ref HEAD --show-diff-on-failure
     - uv run --dev --no-cache ruff check --output-format=gitlab > gl-code-quality-report.json
-
-    #testing & code coverage
-    - echo "The current branch is $FROM_REF"
-    - uv run --group test --no-cache bash -c "
-      coverage erase &&
-      export DJANGO_SETTINGS_MODULE=django_project.settings.local &&
-      python manage.py migrate &&
-      coverage run -m pytest tests/unit tests/functional --junitxml=report_test.xml &&
-      coverage report --fail-under=\"${COVERAGE_MIN_PC}\" &&
-      coverage xml -i &&
-      coverage html"
+    # Testing & code coverage
+    - |
+      uv run --group test --no-cache bash -c "
+        coverage erase &&
+        export DJANGO_SETTINGS_MODULE=django_project.settings.local &&
+        python manage.py migrate &&
+        coverage run -m pytest tests/unit tests/functional --junitxml=report_test.xml &&
+        coverage report --fail-under=\"${COVERAGE_MIN_PC}\" &&
+        coverage xml -i &&
+        coverage html"
   artifacts:
     expire_in: 30 days
     reports:
@@ -93,25 +106,16 @@ all_tests:
       junit: report_test.xml
     paths:
       - coverage.xml
-      - report.xml
-      - report.html
+      - report_test.xml
       - htmlcov/
-      - pylint-report.txt
   needs: []
-  rules:
-      # don't run on master during merging
-      - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
-        when: never
-      # run on branches
-      - if: $CI_COMMIT_BRANCH
+  <<: *test_jobs_rules # Apply rules for test jobs
 
 test_sonar:
   stage: test
-  needs: [ ]
+  needs: []
   image: "${SONAR_SCANNER_IMAGE}"
   allow_failure: true
-  before_script: # to override the global before_script
-    - 'true'
   script:
     - sonar-scanner -X
       -Dsonar.host.url=${CENTRAL_SONAR_URL}
@@ -129,34 +133,26 @@ test_sonar:
       -Dsonar.python.pylint.reportPath=pylint-report.txt
       -Dsonar.scm.disabled=False
       -Dsonar.scm.provider=git
-  rules:
-      # don't run on master during merging
-      - if: $CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH && $CI_COMMIT_MESSAGE =~ /^Merge branch/
-        when: never
-      # run on branches
-      - if: $CI_COMMIT_BRANCH
+  <<: *test_jobs_rules # Apply rules for test jobs
 
-
+# ==========================================================================
+# Release Stage Jobs (run on merge to master)
+# ==========================================================================
 pre_release_last_check:
   stage: release
   image: p-3096-docker-local.artifactory-dogen.group.echonet/icpy/code-quality:uv
   script:
-    #code quality
-    - FROM_REF=$CI_DEFAULT_BRANCH
-    - echo "The current branch is $(git branch --show-current)"
-    - git fetch origin $FROM_REF
-    # last testing before release after the merge
-    - uv run --group test --no-cache bash -c "
-      export DJANGO_SETTINGS_MODULE=django_project.settings.local &&
-      python manage.py migrate &&
-      coverage run -m pytest tests/unit tests/functional"
-  needs: [ ]
-  only:
-    - master
+    # Final test run on the merged code before triggering the release
+    - |
+      uv run --group test --no-cache bash -c "
+        export DJANGO_SETTINGS_MODULE=django_project.settings.local &&
+        python manage.py migrate &&
+        coverage run -m pytest tests/unit tests/functional"
+  needs: []
+  <<: *release_jobs_rules # Apply rules for release jobs
 
 trigger_jenkins:
   stage: release
-
   script:
     - >
       set -xv &&
@@ -174,9 +170,5 @@ trigger_jenkins:
           \"MARVIN_DEV_ENV\":\"$MARVIN_DEV_ENV\",
           \"ENABLE_RELEASE_MARVIN\":\"$ENABLE_RELEASE_MARVIN\"
         }" $JENKINS_URL/generic-webhook-trigger/invoke?token=$JENKINS_MARVIN_TOKEN
-  only:
-    - master
   needs:
-    - pre_release_last_check
-#  needs:
-#    - job: semantic_release
+    - pre_

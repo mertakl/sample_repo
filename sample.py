33771ca0bdc138ec16ffd89c12c5c2f7adea0e67
Mert Akel <akelmert95@gmail.com>
2025-12-09 13:48:01 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,3 +4,16 @@ import pytest
 def set_django_settings():
     import os
     os.environ["DJANGO_SETTINGS_MODULE"] = "project_a.settings"
+
+
+[pytest]
+DJANGO_SETTINGS_MODULE =
+testpaths =
+    project_a/tests
+    project_b/tests
+
+markers =
+    project_a: Tests for Project A (uses project A settings)
+    project_b: Tests for Project B (uses project B settings)
+
+

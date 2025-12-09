3ce3b338b6d078723e99064bcb5387bf45040a73
Mert Akel <akelmert95@gmail.com>
2025-12-09 14:10:28 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,5 @@
-# project1/conftest.py
 import os
-import django
-from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1.settings')
-
-if not settings.configured:
-    django.setup()
+def pytest_pycollect_makeitem(collector, name, obj):
+    # Configure Django settings early
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_a.settings")
